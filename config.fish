51d77563b7afd7f9aa66c0bcb0b95ea944b999a0
vccrurial <72652534+vccrurial@users.noreply.github.com>
2022-12-09 09:27:09 -0800

    Add files via upload

--- config.fish
+++ config.fish
@@ -2,17 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source ~/.config/aliases
+source ~/.config/shell/aliases
+source ~/.config/shell/profile
+source /etc/profile.d/plan9.sh
 set fish_greeting
 fish_vi_key_bindings
-
-setenv XDG_DATA_HOME "$HOME/.local/share"
-setenv XDG_CONFIG_HOME "$HOME/.config"
-setenv XDG_STATE_HOME "$HOME/.local/state"
-setenv XDG_CACHE_HOME "$HOME/.cache"
-setenv XDG_RUNTIME_DIR "/run/user/1000"
-setenv VIS_PATH "$XDG_CONFIG_HOME/vis/"
-
 # fish_add_path /home/vcc/.local/bin
 # fish_add_path /opt/texlive/2022/bin/x86_64-linux
 # fish_add_path /home/vcc/void-packages
